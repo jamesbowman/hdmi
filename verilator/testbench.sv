a37d4e211b9b7a775c40af2b9a05b647cc883ccc
James Bowman <jamesb@excamera.com>
2020-07-19 08:27:25 -0700

    Confirm audio sample flow

--- verilator/testbench.sv
+++ verilator/testbench.sv
@@ -5,8 +5,6 @@ module testbench(
     wire clk_pixel = clock;
     reg [23:0] rgb = 24'h112233;
     wire [15:0] audio_sample_word [1:0];
-    assign audio_sample_word[0] = 16'hA947;
-    assign audio_sample_word[1] = 16'hA946;
     
     reg [31:0] d = 0;
     wire [31:0] dInc = d[31] ? (48000) : (48000 - 74250000);
@@ -16,6 +14,14 @@ module testbench(
       d = dN;
     end
     wire clk_audio = ~d[31];
+
+    reg [15:0] sampleL = 16'h1111, sampleR = 16'h2222;
+    always @(posedge clk_audio) begin
+      sampleL <= sampleL + 16'h0137;
+      sampleR <= sampleR + 16'h9471;
+    end
+    assign audio_sample_word[0] = sampleL;
+    assign audio_sample_word[1] = sampleR;
 
     /* verilator lint_off PINMISSING */
     hdmi #(
